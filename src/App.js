4647f6b6f3a74003af4b16f2e31c2222d1f75514
Zach Carrera <zachcarrera@gmail.com>
2023-03-13 18:39:09 -0700

    add fetch with axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [pokemonList, setPokemonList] = useState([]);
+
+    const getPokemon = () => {
+        axios
+            .get("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
+            .then((response) => setPokemonList(response.data.results));
+    };
+
+    return (
+        <div className="App">
+            <button onClick={() => getPokemon()}>Fetch Pokemon</button>
+            <ul>
+                {pokemonList.map((pokemon, index) => (
+                    <li key={index} className="list-element">
+                        {pokemon.name}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
